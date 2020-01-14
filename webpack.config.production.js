96f784c87fc625f6a51555fe522b429fe8b6ff2c
André Monello <atmonello@gmail.com>
2020-01-13 22:25:40 -0300

    Adding production config for Webpack

---
+++ webpack.config.production.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000,
+            name: 'assets/img/[name].[ext]',
+          },
+        }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: path.resolve('public', 'index.html'),
+      title: 'Banner HTML',
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        ignoreCustomComments: true,
+        minifyCSS: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeTagWhitespace: true,
+      },
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+};
